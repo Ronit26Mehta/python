5eb13e56972bdd1a21f18b48b1d165214cf05fa7
RonitMehta260704 <109467924+RonitMehta260704@users.noreply.github.com>
2023-02-25 22:59:22 +0530

    Add files via upload

---
+++ python crap/expirements programs/6_prac/prac_6_exercise_2.py
@@ -0,0 +1,6 @@
+print("the lists")
+tot=1
+l=[10,20,3,40,6]
+for x in l:
+    tot*=x
+print("the list mult is",tot)
---
+++ python crap/expirements programs/6_prac/prac_6_exercise_4.py
@@ -0,0 +1,3 @@
+print("the list min")
+l=[100,20,34,600,34,1]
+print("the min value",min(l))

a85a12e237102e190c6ef9a2a9380f56d1577ea1
RonitMehta260704 <109467924+RonitMehta260704@users.noreply.github.com>
2023-03-04 13:30:38 +0530

    Delete python crap/expirements programs/13_prac directory

--- python crap/expirements programs/13_prac/prac_13_exercise_2.py
+++
@@ -1,8 +0,0 @@
-import numpy
-import numpy as np
-a=np.array(["Ronit"], dtype = np.str)
-b=np.array(["mehta"], dtype = np.str)
-print("the first name:-",a)
-print("the last name:-",b)
-full_name = np.char.add(a, b)
-print(full_name)

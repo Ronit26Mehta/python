9adab8e59cf7884630d9037b4823214fa6ee54b9
Mr.House <109467924+MrHouse2020@users.noreply.github.com>
2023-04-05 17:22:13 +0530

    Add files via upload

---
+++ python crap/assingment_3/dictionary_solution.py
@@ -0,0 +1,7 @@
+
+student = {1:"ronit",2:"harsh",3:"shardul"}
+print(student)
+student[2]= "shreyas"
+print(student)
+del student[1]
+print(student)
---
+++ python crap/assingment_3/output_2.py
@@ -0,0 +1,13 @@
+a  = [2,5,1,3,6,9,7]
+a[2:6]= [2,4,9,0]
+print(a)
+print("___________")
+b = ["Hello","World"]
+b.append("Python")
+print(b)
+print("__________")
+t1 = [3,5,6,7]
+print(t1[2])
+print(t1[-1])
+print(t1[2:])
+print(t1[:])
